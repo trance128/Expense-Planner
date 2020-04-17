344fcb85c675c7ca2311ce83c8c6dc1447fb4ca2
Trance128 <Trance128@github.com>
2020-04-17 03:01:02 +0100

    Added Transaction class

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() => runApp(App());
 
@@ -13,14 +14,30 @@ class App extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(id: "1", title: "Flutter course", amount: 19.99, date: DateTime.now(), ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Expense Planner"),
       ),
-      body: Center(
-        child: Text("Placeholder Text"),
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text("CHART"),
+              elevation: 5,
+            ),
+          ),
+          Card(
+            child: Text("List of transactions"),
+          ),
+        ],
       ),
     );
   }
