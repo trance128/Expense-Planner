2b5905e9f1eb93ba46f94e5c45aedec07c8416c1
Trance128 <Trance128@github.com>
2020-04-17 01:33:19 +0100

    Initializing git, empty project

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Expense Planner',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Expense Planner"),
+      ),
+      body: Center(
+        child: Text("Placeholder Text"),
+      ),
+    );
+  }
+}
